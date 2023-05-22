99030e7590d411dde2090d792b0f6bb600ea4658
masaiharshit <harshit007.ara@gmail.com>
2023-05-22 09:32:30 +0530

    done

--- prac-2/src/Components/Sidebar.jsx
+++ prac-2/src/Components/Sidebar.jsx
@@ -1,35 +1,27 @@
-import styled from "styled-components";
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
 import { getMatches } from "../Redux/Matches/action";
+
 export const Sidebar = () => {
-  const [order,setOrder]=useState("")
-  const [page,setPage]=useState(1)
-  const handleSort=(e)=>{
-   const value=e.target.value
-   setOrder(value)
-  }
-  
- 
-  const handlePage = (val) => {
-    setPage((prev) => prev + val);
-  };
   const dispatch=useDispatch()
+  const [year, setYear]=useState("")
+  const [order, setOrder]=useState("")
+  const [page, setPage]=useState(1)
   useEffect(()=>{
-    const paramObj={
-      params:{
-         _sort:"year",
-         _order:order,
-         page,
-      }
-    }
-    dispatch(getMatches(order&&paramObj&&page))
-},[order])
+    dispatch(getMatches(page,order,year))
+  },[page,order])
+  const handleChange=(e)=>{
+    setOrder(e.target.value)
+    setYear("year")
+  }
   return (
     <DIV>
       <h3>Sort By Year</h3>
-      <div onChange={handleSort}>
-        <input data-testid="sort-asc" type="radio" name="sort" value={"asc"} defaultChecked={order==="asc"}/>
+      <div>
+        <input data-testid="sort-asc" type="radio" name="sort" value={"asc"}
+        onChange={(e)=>handleChange(e)} />
+         
         <label>Ascending</label>
         <br />
         <br />
@@ -38,6 +30,7 @@ export const Sidebar = () => {
           type="radio"
           name="sort"
           value={"desc"}
+          onChange={(e)=>handleChange(e)}
         />
         <label>Descending</label>
       </div>
@@ -46,10 +39,10 @@ export const Sidebar = () => {
       <br />
       <br />
       <h3 data-testid="page-number">Page No.{page} </h3>
-      <button data-testid="previous-page" disabled={page === 1} onClick={()=>handlePage(-1)}>Previous</button>
+      <button data-testid="previous-page" onClick={()=>setPage((prev)=>prev-1)}>Previous</button>
       <br />
       <br />
-      <button data-testid="next-page"  onClick={()=>handlePage(+1)}>Next</button>
+      <button data-testid="next-page" onClick={()=>setPage((prev)=>prev+1)}>Next</button>
     </DIV>
   );
 };
--- prac-2/src/Redux/Matches/action.js
+++ prac-2/src/Redux/Matches/action.js
@@ -1,21 +1,30 @@
+import axios from "axios";
 import { GET_MATCH_FAILURE, GET_MATCH_REQUEST, GET_MATCH_SUCCESS } from "./actionTypes";
-import axios from "axios"
-const MatchesRequest=()=>{
-  return {type:GET_MATCH_REQUEST}
-}
-const MatchesSuccess=(payload)=>{
+
+const getdatasuccess=(payload)=>{
   return {type:GET_MATCH_SUCCESS,payload}
-}
-const MatchesFailure=()=>{
-  return {type:GET_MATCH_FAILURE}
+
 }
 
-export const getMatches = (paramObj)=>(dispatch) => {
-  dispatch(MatchesRequest());
-  axios.get(`http://localhost:${process.env.REACT_APP_JSON_SERVER_PORT}/matches`,paramObj).then((res)=>{
-    dispatch(MatchesSuccess(res.data))
-    return res.data
-  }).catch(()=>{
-    dispatch(MatchesFailure())
-  })
+const getdatarequest=()=>{
+  return {type:GET_MATCH_REQUEST}
+}
+
+const getdatafailure=()=>{
+  return {type: GET_MATCH_FAILURE}
+}
+
+export const getMatches = (pgno,sort,year) =>(dispatch)=> {
+  // Complete the logic
+  dispatch(getdatarequest())
+  return axios
+  .get(
+    ` http://localhost:${process.env.REACT_APP_JSON_SERVER_PORT}/matches?_limit=10&_page=${pgno}&_sort=${year}&_order=${sort}`
+  )
+.then((res)=>{
+  dispatch(getdatasuccess(res.data))
+})
+.catch((err)=>{
+  dispatch(getdatafailure)
+})
 };
